3d6871c4f4d84387888c005110efbfe9dba41f94
macksfield <maxfieldmccall@gmail.com>
2021-02-03 16:27:01 -0800

    adding project description and link to github

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,13 @@ import './styles.scss'
 
 const Header = () =>
     <div>
-      <header id="header">
-          <h1>React, Router, Redux, Redux-Thunk Example Project Architecture</h1>
-      </header>
+        <header id="header">
+            <a href="/"><h1>React, Router, Redux, Redux-Thunk Example Project Architecture</h1></a>
+            <p>This project is designed to service as a template for a ReactJS project using the above technologies.</p>
+            <p>Users can switch between a list and a grid view, and view an image gallery when clicking individual listings.</p>
+            <a href="https://github.com/macksfield/Realestate-Demo">GitHub Project</a>
+        </header>
+        <br/>
     </div>
 
 export default Header
