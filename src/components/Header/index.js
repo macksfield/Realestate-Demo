dfc6628cca0ef2ea95ed0e9225e30eafe6c6e5d8
macksfield <maxfieldmccall@gmail.com>
2021-02-03 16:33:36 -0800

    adding blurb to ignore lack of styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () =>
     <div>
         <header id="header">
             <a href="/"><h1>React, Router, Redux, Redux-Thunk Example Project Architecture</h1></a>
-            <p>This project is designed to service as a template for a ReactJS project using the above technologies.</p>
+            <p>This project is designed to service as a template for a ReactJS project using the above technologies. Please ignore the lack of styling, this is an architectural example - see GitHub for how it is built.</p>
             <p>Users can switch between a list and a grid view, and view an image gallery when clicking individual listings.</p>
             <a href="https://github.com/macksfield/Realestate-Demo">GitHub Project</a>
         </header>
